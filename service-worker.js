8239a2ff95e51e61111741c2ab7411b095963eef
chalkenm <mm30031@students.mcpasd.k12.wi.us>
2025-03-17 14:00:45 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('pwa-cache').then((cache) => {
+            return cache.addAll([
+                'index.html',
+                'style.css',
+                'script.js',
+                'manifest.json',
+                'audio/music.mp3'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
